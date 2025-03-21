8d36d40914a3813f4f2f104798f459541a658526
Matheus Sousa <matheussousab2014@gmail.com>
2025-03-21 14:46:50 -0300

    feat: adicionar vite-plugin-compression para otimização de arquivos

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      deleteOriginFile: false,
+      filter: (file) => {
+        return /\.(js|css|html|svg)$/.test(file);
+      },
+    })
   ],
 })
